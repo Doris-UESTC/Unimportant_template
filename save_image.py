60609d10fee5a8bde014b8300f4c0df4814643ef
Uestc_doris <56374719+Doris-UESTC@users.noreply.github.com>
2022-12-06 17:11:11 +0800

    多线程运行脚本

---
+++ save_image.py
@@ -0,0 +1,18 @@
+#tensor保存图片
+import torch
+from torchvision import utils as vutils
+def tensor_saveimg(tensor, filename):
+    """
+    将tensor保存为图片
+    :param input_tensor: 要保存的tensor
+    :param filename: 保存的文件名
+    """
+    assert (len(input_tensor.shape) == 4 and input_tensor.shape[0] == 1)
+    # 复制一份
+    input_tensor = input_tensor.clone().detach()
+    # 到cpu
+    input_tensor = input_tensor.to(torch.device('cpu'))
+    # 反归一化
+    # input_tensor = unnormalize(input_tensor)
+    vutils.save_image(input_tensor, filename)
+
